ff3f94ec984a1be5be47e006164dbbee3a2e773f
meetdobaiya <161558921+meetdobaiya@users.noreply.github.com>
2024-04-02 18:20:43 +0530

    Update main.cpp with new code

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
-#include<bits/stdc++.h>
-#include <fstream>
-
-using namespace std;
-
-#include "trie.cpp"
-
+#include"dictionary.h"
 
 int main() {
-    ifstream inputfile("words_alpha.txt");
-    trie strie;
-
-    if(inputfile.is_open()){
-        cout << "File opened successfully.\n";
-
-        string word;
-
-        while( getline(inputfile , word) )
-            strie.insertWord(word);
-
-    }
-
-    cout << ( strie.searchWord("a") ? "FOUND" : "NOT FOUND" ) << endl;
-
+    spellchecker f1;
+    f1.checking_file("input.txt");
+    f1.replace_wrong_spell("input.txt");
+    
     return 0;
 }
