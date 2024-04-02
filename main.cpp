eee9d1af8e9c8cafb7d6e24c83868bb9c9190eb5
meetdobaiya <161558921+meetdobaiya@users.noreply.github.com>
2024-04-02 18:58:59 +0530

    Update main.cpp to improve user experience

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#include"dictionary.h"
+#include "dictionary.h"
+#include <string>
 
-int main() {
+int main()
+{
+    // create object of class and call constructor
     spellchecker f1;
+
+    // Get input file name from user
+    cout << "Enter File Name to check spelling\n";
+    string file_name;
+    getline(cin, file_name);
+    cout << " ~> Following underlined words are wrong words\n";
     f1.checking_file("input.txt");
+    cout << " ~> wrong words have been corrected in the " << file_name << " yon can check in the file \n";
     f1.replace_wrong_spell("input.txt");
-    
+
     return 0;
 }
