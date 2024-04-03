5daf00d8b5ce5c2008e859d0f8dd6907777b31d0
meetdobaiya <161558921+meetdobaiya@users.noreply.github.com>
2024-04-03 23:11:57 +0530

    Add files via upload

---
+++ HIGHLIGHT.h
@@ -0,0 +1,14 @@
+#define underlineON "\033[4m"
+#define underlineOFF "\033[0m"
+
+#define redlineON "\033[1;31m"
+#define redlineOFF "\033[0m"
+
+#define bluelineON "\033[1;34m"
+#define bluelineOFF "\033[0m"
+
+#define greenlineON "\033[1;32m"
+#define greenlineOFF "\033[0m"
+
+#define yellowlineON "\033[1;33m"
+#define yellowlineOFF "\033[0m"
